25b4cf8cccbedbd23238964cbcbb8eb2388dda40
Jakub <slomkas@gmail.com>
2019-05-24 10:22:04 +0200

    Fix

--- favnote/src/App.js
+++ favnote/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import "./App.css";
 
-class App = () => (
+const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
