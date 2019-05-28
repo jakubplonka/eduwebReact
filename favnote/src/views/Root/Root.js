c4c09abb281d3d9461589d3a57a2944943f59af2
Jakub <slomkas@gmail.com>
2019-05-28 17:15:06 +0200

    Setup + husky + css

---
+++ favnote/src/views/Root/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
+
+const Root = () => (
+  <div>
+    <GlobalStyle />
+    <h1>adasd</h1>
+    <Button>CLOSE / SAVE</Button>
+    <Button secondary>Remove</Button>
+  </div>
+);
+
+export default Root;
