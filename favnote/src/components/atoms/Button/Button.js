0142db4effca93b212b53daf19cc664702bce5c0
jakub.plonka <jakub.plonka@schibsted.com>
2019-07-12 15:44:14 +0200

    cards

--- favnote/src/components/atoms/Button/Button.js
+++ favnote/src/components/atoms/Button/Button.js
@@ -1,12 +1,12 @@
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: ${({ theme }) => theme.primary};
+  background-color: ${({ theme }) => theme.note};
   width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
-  font-family: "Montserrat";
+  font-family: 'Montserrat';
   font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
